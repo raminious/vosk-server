664d3ed75cc9bcf3907ff33914d98dd532cdcd94
Peter Svistunov <svistunov.p@i20.biz>
2021-12-23 07:45:20 +0800

    Now this code work as real API.

--- app.py
+++ app.py
@@ -1,45 +1,67 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from tasks import recognize
+from os import remove
 import json
+import tempfile
 
 api = Flask(__name__)
 
 
 @api.route('/asr/recognize', methods=['POST'])
 def transcribe():
-  # an example for passing data to the task
-  data = {
-      'filepath': './files/test.wav'
-  }
+    if request.content_type != 'audio/x-wav':
+        return jsonify({
+            'msg': "415 Unsupported Media Type ;)"
+        }), 415
 
-  task = recognize.apply_async(args=[data])
+    data = None
+    if request.headers.get('Transfer-Encoding') == 'chunked':
+        data = request.stream
+    elif request.data:
+        data = request.data
 
-  return {
-      'id': task.id
-  }
+    if data is None:
+        return jsonify({
+            'msg': "400 No data found ;)"
+        }), 400
+
+    fp = tempfile.NamedTemporaryFile(delete=False)
+    fp.write(data)
+
+    task = recognize.apply_async(args=[{
+        'filename': fp.name
+    }])
+
+    return jsonify({
+        'id': task.id
+    })
 
 
 @api.route('/asr/recognize/status/<task_id>', methods=['GET'])
 def result(task_id):
-  task = recognize.AsyncResult(task_id)
-  if task.state == 'PENDING':
-    response = {
-        'state': task.state,
-    }
-  elif task.state == 'SUCCESS':
-    response = {
-        'state': task.state,
-        'data': json.loads(task.info)
-    }
+    task = recognize.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+        }
+    elif task.state == 'SUCCESS':
+        try:
+            remove(task.args[0]['filename'])
+        except FileNotFoundError:
+            pass
+        response = {
+            'state': task.state,
+            'data': json.loads(task.info)
+        }
 
-  else:
-    response = {
-        'state': task.state,
-        'status': str(task.info)
-    }
+    else:
+        response = {
+            'state': task.state,
+            'status': str(task.info)
+        }
 
-  return jsonify(response)
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-  api.run()
+    api.run()
